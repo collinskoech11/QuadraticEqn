f1ffa2ab1b108448dacef230020e51431b567c63
collinskoech11 <collinskoechck34@gmail.com>
2020-08-22 12:03:50 +0300

    second commit

--- QuadratioEqn.java
+++ QuadratioEqn.java
@@ -13,5 +13,14 @@ public class QuadratioEqn {
         double c = sc.nextDouble();
 
         double determinant = (b*b)-(4*a*c);
+        double sqrt = Math.sqrt(determinant);
+
+        if(determinant>0){
+            firstRoot = (-b+sqrt)/(2*a);
+            secondRoot =(-b - sqrt)/(2*a);
+            System.out.println("Roots are :"+firstRoot+"and"+secondRoot);
+        }else if(determinant == 0){
+            System.out.println("Root is :"+(-b+sqrt)/(2*a));
+        }
     }
 }
