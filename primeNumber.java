3aaa71aa7ccd82557db40faae5cf30b938c7cc6a
collinskoech11 <collinskoechck34@gmail.com>
2020-08-26 12:06:39 +0300

    java calculator

--- primeNumber.java
+++ primeNumber.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class primeNumber {
     public static void main(String []args){
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("NUmber to be checked");
+        int n = sc.nextInt();
+        int i=2;
+        boolean flag =false;
+        while(i <= n/2)
+        {
+            // condition for nonprime number
+            if(n % i == 0)
+            {
+                flag = true;
+                break;
+            }
+
+            ++i;
+        }
+
+        if (!flag){
+            System.out.println(n + " is a prime number.");
+        }else{
+            System.out.println(n + " is not a prime number.");
+        }
     }
 }
